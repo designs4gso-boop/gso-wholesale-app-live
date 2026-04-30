8e1f2fd60393fa14badb09c88dc972fc69c61655
designs4gso-boop <designs4gso@gmail.com>
2026-04-30 13:22:50 -0700

    add create order route

--- app/routes.ts
+++ app/routes.ts
@@ -14,6 +14,7 @@ export default [
     route("wholesale/calculator", "routes/app.wholesale.calculator.tsx"),
     route("quotes", "routes/app.quotes.tsx"),
     route("product-costs", "routes/app.product-costs.tsx"),
+    route("create-order", "routes/app.create-order.tsx"),
   ]),
 
   route("apps/wholesale-lite", "routes/apps.wholesale-lite._index.ts"),
