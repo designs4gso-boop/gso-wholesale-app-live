be3ac538ac5e1ef40bbdfe78f3d50c4450430c94
designs4gso-boop <designs4gso@gmail.com>
2026-04-29 17:52:06 -0700

    fix prisma export

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const db =
+const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["warn", "error"],
+    log: ["query", "error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = db;
+  global.prisma = prisma;
 }
+
+export default prisma;
